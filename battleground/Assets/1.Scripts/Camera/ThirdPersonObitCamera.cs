f69f501aeb42460aef5f561f81bf4f02778d86fa
YangJJHH <73065398+YangJJHH@users.noreply.github.com>
2022-12-09 22:03:22 +0900

    카메라 스크립트

--- battleground/Assets/1.Scripts/Camera/ThirdPersonObitCamera.cs
+++ battleground/Assets/1.Scripts/Camera/ThirdPersonObitCamera.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
+using TreeEditor;
 using UnityEngine;
 
 public class ThirdPersonObitCamera : MonoBehaviour
 {
     public Transform player;
     public Vector3 pivotOffset = new Vector3(0.0f, 1.0f, 0.0f);
-    public Vector3 camOffset = new Vector3(0.4f,0.5f,-2.0f);
+    public Vector3 camOffset = new Vector3(0.4f, 0.5f, -2.0f);
 
     public float smooth = 10f; //카메라 반응속도
     public float horizontalAimingSpeed = 6.0f; //수평 회전 속도
@@ -83,5 +85,87 @@ public class ThirdPersonObitCamera : MonoBehaviour
     public void SetFOV(float customFOV)
     {
         targetFOV = customFOV;
+    }
+
+    bool ViewingPosCheck(Vector3 checkPos, float deltaPlayerHeight)
+    {
+        Vector3 target = player.position + (Vector3.up * deltaPlayerHeight);
+        if (Physics.SphereCast(checkPos, 0.2f, target - checkPos, out RaycastHit hit, relCameraPosMag))
+        {
+            if (hit.transform != player && !hit.transform.GetComponent<Collider>().isTrigger)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    bool ReverseViewingPosCheck(Vector3 checkPos, float deltaPlayerHeight, float maxDistance)
+    {
+        Vector3 origin = player.position + (Vector3.up * deltaPlayerHeight);
+        if (Physics.SphereCast(origin, 0.2f, checkPos - origin, out RaycastHit hit, maxDistance))
+        {
+            if (hit.transform != player && !hit.transform.GetComponent<Collider>().isTrigger && hit.transform != transform)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    bool DoubleViewingPosCheck(Vector3 checkPos, float offset)
+    {
+        float playerFocusHeight = player.GetComponent<CapsuleCollider>().height * 0.75f;
+        return ViewingPosCheck(checkPos, playerFocusHeight) && ReverseViewingPosCheck(checkPos, playerFocusHeight, offset);
+    }
+
+    void Update()
+    {
+        //마우스 이동 값
+        angleH += Mathf.Clamp(Input.GetAxis("Mouse X"), -1f, 1f) * horizontalAimingSpeed;
+        angleV += Mathf.Clamp(Input.GetAxis("Mouse Y"), -1f, 1f) * verticalAimingSpeed;
+        //수직 이동 제한
+        angleV = Mathf.Clamp(angleV, minVerticalAngle, targetMaxVerticalAngle);
+        //수직 카메라 바운스
+        angleV = Mathf.LerpAngle(angleV, angleV + recoilAngle, 10f * Time.deltaTime);
+
+        //카메라 회전
+        Quaternion camYRotation = Quaternion.Euler(0.0f, angleH, 0.0f);
+        Quaternion aimRotation = Quaternion.Euler(-angleV, angleH, 0.0f);
+        cameraTransform.rotation = aimRotation;
+
+        //set Fov
+        myCamera.fieldOfView = Mathf.Lerp(myCamera.fieldOfView, targetFOV, Time.deltaTime);
+
+        Vector3 baseTempPosition = player.position + camYRotation * targetPivotOffset;
+        Vector3 noCollisionOffset = targetCamOffset;
+
+        for (float zOffset = targetCamOffset.z; zOffset <= 0f; zOffset += 0.5f)
+        {
+            noCollisionOffset.z = zOffset;
+            if (DoubleViewingPosCheck(baseTempPosition + aimRotation * noCollisionOffset, Mathf.Abs(zOffset)) || zOffset == 0f)
+            {
+                break;
+            }
+        }
+        //Reposition Camera
+        smoothPivotOffset = Vector3.Lerp(smoothPivotOffset, targetPivotOffset, smooth * Time.deltaTime);
+        smoothCamOffset = Vector3.Lerp(smoothCamOffset, noCollisionOffset, smooth * Time.deltaTime);
+
+        cameraTransform.position = player.position + camYRotation * smoothPivotOffset + aimRotation * smoothCamOffset;
+
+        if(recoilAngle > 0.0f)
+        {
+            recoilAngle -= recoilAngleBounce * Time.deltaTime;
+        }
+        else if (recoilAngle < 0.0f)
+        {
+            recoilAngle += recoilAngleBounce * Time.deltaTime;
+        }
+
+    }
+    public float GetCurrentPivotMagnitude(Vector3 finalPivotOffset)
+    {
+        return Mathf.Abs((finalPivotOffset - smoothPivotOffset).magnitude);
     }
 }

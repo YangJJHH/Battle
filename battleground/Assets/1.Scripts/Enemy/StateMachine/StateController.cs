d63ed945ac27eb6936c63e98a8074c222e84504a
Jaehyeock <73065398+YangJJHH@users.noreply.github.com>
2022-12-27 17:31:20 +0900

    Enemy FSM 초기세팅

---
+++ battleground/Assets/1.Scripts/Enemy/StateMachine/StateController.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+/// <summary>
+/// 
+/// state -> actions update -> transition check
+/// state에 필요한 기능들. 애니메이션 콜백들.
+/// 시야 체크 , 찾아논 엄폐물 장소중 가장 가까운 위치를 찾는 기능
+/// </summary>
+public class StateController : MonoBehaviour
+{
+    public GenericStats genericStats;
+    public ClassStats statData;
+    public string classID;
+    public ClassStats.Param classStats 
+    {
+        get
+        {
+            foreach(ClassStats.Sheet sheet in statData.sheets)
+            {
+                foreach (ClassStats.Param param in sheet.list)
+                {
+                    if (param.ID.Equals(classID))
+                    {
+                        return param;
+                    }
+                }
+            }
+            return null;
+        }
+    }
+
+    public State currentState;
+    public State remainState;
+
+    public Transform aimTarget;
+    public List<Transform> patrolWayPoints;
+    public int bullets;
+    [Range(0,50)]
+    public float viewRadius;
+    [Range(0, 360)]
+    public float viewAngle;
+    [Range(0, 25)]
+    public float perceptionRadius;
+
+    [HideInInspector]public float nearRadius;
+    [HideInInspector]public NavMeshAgent nav;
+    [HideInInspector]public int wayPointIndex;
+    [HideInInspector]public int maximumBurst = 7;
+
+    [HideInInspector]public float blindEngageTime = 30f; //대상이 시야에서 멀어지고 일정시간동안 대상을 찾는시간
+    [HideInInspector]public bool targetInsight;
+    [HideInInspector]public bool focusSight;
+    [HideInInspector]public bool hadClearshot; // 과거에
+    [HideInInspector]public bool haveClearShot; //현재
+    [HideInInspector]public int coverHash = -1; //각각 다른 장애물에 숨기위한 고유 해쉬값
+    
+    [HideInInspector]public EnemyVariables variables;
+    [HideInInspector]public Vector3 personalTarget = Vector3.zero;
+
+    private int magBullets;
+    private bool aiActive;
+    private static Dictionary<int, Vector3> coverSpot; // ai들이 공유할 장애물 점유상황
+    private bool straging;
+    private bool aiming;
+    private bool checkedOnLoop, blockedSight;
+
+    [HideInInspector] public EnemyAnimation enemyAnimation;
+    [HideInInspector] public CoverLookUp coverLookUp;
+
+    public Vector3 CoverSpot 
+    {
+        get { return coverSpot[GetHashCode()]; } //ai마다 고유한 해쉬코드
+        set { coverSpot[GetHashCode()] = value; }
+    }
+
+    public void TransitionToState(State nextState, Decision decision)
+    {
+        if(nextState != remainState)
+        {
+            currentState = nextState;
+        }
+    }
+}
---
+++ battleground/Assets/1.Scripts/GameData/GenericStats.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName="PluggableAI/GeneralStats")]
+public class GenericStats : ScriptableObject
+{
+    [Header("Genereal")]
+    [Tooltip("NPC정찰 속도 clear state")]
+    public float patrolSpeed = 2f;
+    [Tooltip("NPC가 따라오는속도")]
+    public float chaseSpeed = 5f;
+    [Tooltip("npc 회피하는 속도 engage State")]
+    public float evadeSpeed = 15f;
+    [Tooltip("웨이포인트에서 대기하는 시간")]
+    public float patrolWaitTime = 2f;
+    [Header("Animation")]
+    [Tooltip("장애물 레이어 마스크")]
+    public LayerMask obstacleMask;
+    [Tooltip("조준시 버벅임을 피하기 위한 최소 앵글")]
+    public float angleDeadZone = 5f;
+    [Tooltip("속도 댐핑 시간")]
+    public float speedDampTime = 0.4f;
+    [Tooltip("회전속도에 대한 댐핑시간")]
+    public float angularSpeedDampTime = 0.2f;
+    [Tooltip("각속도 안에서 각도회전에 따른 반응 시간")]
+    public float angleResponseTime = 0.2f;
+    [Header("Cover")]
+    [Tooltip("캐릭터가 숨을 수 있는 장애물의 최소 높이값")]
+    public float aboveCoverHeight = 1.5f;
+    [Tooltip("숨을수 있는 장애물 레이어 마스크")]
+    public LayerMask coverMask;
+    [Tooltip("총을 쏠수 있는 마스크")]
+    public LayerMask shotMask;
+    [Tooltip("타겟(플레이어)")]
+    public LayerMask targetMask;
+
+}
